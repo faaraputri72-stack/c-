aef5bb64170d5334ebc762bd6ee1f8f983375242
Fara <faaraputri72@gmail.com>
2025-10-18 12:37:03 +0700

    Add basic calculator program in kalkulator.cpp

---
+++ kalkulator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka1, angka2, hasil;
+
+    cout << "Masukkan angka pertama: ";
+    cin >> angka1;
+
+    cout << "Masukkan angka kedua: ";
+    cin >> angka2;
+
+    hasil = angka1 + angka2;
+
+    cout << "Hasil penjumlahan: " << hasil << endl;
+
+    return 0;
+}
+
